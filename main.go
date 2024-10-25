edea50fabed298ec6dd8a252a49aba32027cbc52
Em Evgeniy <emevgeniy1323@gmail.com>
2024-10-25 22:43:44 +0500

    add read html file in about route

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+
 	"log"
 	"net/http"
 	"os"
@@ -14,7 +15,17 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "About handler")
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	htmlContent, err := os.ReadFile("about.html")
+	if err != nil {
+		http.Error(w, "Не удалось загрузить страницу", http.StatusInternalServerError)
+		log.Printf("Ошибка чтения файла: %v", err)
+		return
+	}
+
+	w.Write(htmlContent)
 }
 
 func main() {
@@ -32,7 +43,7 @@ func main() {
 	http.HandleFunc("/about", aboutHandler)
 
 	fmt.Printf("Сервер запущен на порту %v \n", port)
-	err = http.ListenAndServe(":"+port, nil) // Используем порт из переменных окружения
+	err = http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal("Ошибка запуска сервера:", err)
 	}
